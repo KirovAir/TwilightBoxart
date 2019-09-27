c6c725b1a0ecf23a1c022c6b6b4e4e8c2d84e546
Jesse <info@jessesander.nl>
2019-09-27 12:17:14 +0200

    Finally fixed win7 SSL framework compatibility.

--- TwilightBoxart.UX/Program.cs
+++ TwilightBoxart.UX/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Windows.Forms;
 
 namespace TwilightBoxart.UX
@@ -11,6 +12,37 @@ namespace TwilightBoxart.UX
         [STAThread]
         static void Main()
         {
+            // Possible windows XP / 7 / older .net framework compatibility fix.
+            try
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)48 // SSl3
+                                                       | (SecurityProtocolType)192 // tls
+                                                       | (SecurityProtocolType)768 // tls11
+                                                       | (SecurityProtocolType)3072 // tls12
+                                                       | (SecurityProtocolType)12288; // tls13
+            }
+            catch
+            {
+                try // Try without tls13
+                {
+                    ServicePointManager.SecurityProtocol = (SecurityProtocolType)48 // SSl3
+                                                           | (SecurityProtocolType)192 // tls
+                                                           | (SecurityProtocolType)768 // tls11
+                                                           | (SecurityProtocolType)3072; // tls12
+                }
+                catch
+                {
+                    try // Try without tls12
+                    {
+                        ServicePointManager.SecurityProtocol = (SecurityProtocolType)48 // SSl3
+                                                               | (SecurityProtocolType)192 // tls
+                                                               | (SecurityProtocolType)768; // tls11
+                    }
+                    catch { }
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
